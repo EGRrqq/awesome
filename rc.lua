c426db72febc93e2043de072d6473e9a783fa33a
EGRrqq <egrrqqdev@gmail.com>
2024-10-11 09:30:22 -0400

    change layout order
    move navbar to the bottom

--- rc.lua
+++ rc.lua
@@ -71,22 +71,22 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.floating,
+	awful.layout.suit.spiral,
+	awful.layout.suit.max,
+	awful.layout.suit.spiral.dwindle,
 	awful.layout.suit.tile,
 	awful.layout.suit.tile.left,
 	awful.layout.suit.tile.bottom,
 	awful.layout.suit.tile.top,
 	awful.layout.suit.fair,
 	awful.layout.suit.fair.horizontal,
-	awful.layout.suit.spiral,
-	awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
-	awful.layout.suit.max.fullscreen,
 	awful.layout.suit.magnifier,
 	awful.layout.suit.corner.nw,
-	-- awful.layout.suit.corner.ne,
-	-- awful.layout.suit.corner.sw,
-	-- awful.layout.suit.corner.se,
+	awful.layout.suit.corner.ne,
+	awful.layout.suit.corner.sw,
+	awful.layout.suit.corner.se,
+	awful.layout.suit.max.fullscreen,
+	awful.layout.suit.floating,
 }
 -- }}}
 
@@ -243,7 +243,7 @@ awful.screen.connect_for_each_screen(function(s)
 	})
 
 	-- Create the wibox
-	s.mywibox = awful.wibar({ position = "top", screen = s })
+	s.mywibox = awful.wibar({ position = "bottom", screen = s })
 
 	-- Add widgets to the wibox
 	s.mywibox:setup({
